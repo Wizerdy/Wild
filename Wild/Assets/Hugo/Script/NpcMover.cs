b3b634434eda32a20285aa4e68c41d3f735e4159
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-10-01 13:24:21 +0200

    NavMesh Début

---
+++ Wild/Assets/Hugo/Script/NpcMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMover : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject InitTarget;
+    public bool Chasing;
+    private NavMeshAgent NavA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavA = gameObject.GetComponent<NavMeshAgent>();
+        Chasing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Chasing)
+        {
+            NavA.destination = Target.transform.position;
+        }
+        else
+        {
+            NavA.destination = InitTarget.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Chasing = false;
+        }
+        if (other.tag == "Lair")
+        {
+            Chasing = true;
+        }
+    }
+}
