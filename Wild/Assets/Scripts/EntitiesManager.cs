f752a2a11c814bd64acd2d1709016a25135d1b72
wiredii <grancru2000@gmail.com>
2020-10-01 12:36:49 +0200

    Architecture
    Jérôme, it's for you <3

---
+++ Wild/Assets/Scripts/EntitiesManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EntitiesManager
+{
+    public static List<Entity> entities;
+
+    public static Entity FindEntity(string id)
+    {
+        for (int i = 0; i < entities.Count; i++)
+            if (String.Compare(entities[i].entityId, id) == 0)
+                return entities[i];
+
+        return null;
+    }
+
+    public static Entity[] FindEntities(string group)
+    {
+        List<Entity> entitiesGroup = new List<Entity>();
+
+        for (int i = 0; i < entities.Count; i++)
+            if (String.Compare(entities[i].entityId, group) == 0)
+                entitiesGroup.Add(entities[i]);
+
+        if (entitiesGroup.Count <= 0)
+            return null;
+        else
+            return entitiesGroup.ToArray();
+    }
+
+    public static void AddEntity(Entity entity)
+    {
+        entities.Add(entity);
+    }
+}
---
+++ Wild/Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [Header("Identity")]
+    public string entityId;
+    public string[] entityGroup;
+
+    private void Awake()
+    {
+        EntitiesManager.AddEntity(this);
+    }
+
+    public void MoveTo(Vector3 dir) { }
+
+    public void MoveToDestination(Vector3 dest) { }
+
+    public void Follow(GameObject go) { }
+}
