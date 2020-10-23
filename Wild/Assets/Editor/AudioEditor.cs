deed06df655f61b6437b23c2560f593ed9768e05
loickGombauld <wild1907@hotmail.fr>
2020-10-23 19:21:30 +0200

    Ajout d'un systeme de "Walk" pour le déplacement à la manette

--- Wild/Assets/Editor/AudioEditor.cs
+++ Wild/Assets/Editor/AudioEditor.cs
@@ -6,5 +6,9 @@ using UnityEditor;
 [CustomEditor(typeof(Sound_Manager))]
 public class AudioEditor : Editor
 {
-
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Sound_Manager sm = (Sound_Manager)target;
+    }
 }
--- Wild/Assets/Script_loick/Entity_Controller.cs
+++ Wild/Assets/Script_loick/Entity_Controller.cs
@@ -5,34 +5,42 @@ using UnityEngine;
 public class Entity_Controller : MonoBehaviour
 {
     public Player player;
-
+    public float joyaxeX;
+    public float joyaxeY;
+    public bool test;
+    public float run;
+    public float walk;
+    private void Start()
+    {
+        run = player.speedMax;
+        walk = player.speedMax / 2;
+    }
     void Update()
     {
-        
+        joyaxeX = Input.GetAxis("Horizontal");
+        joyaxeY = Input.GetAxis("Vertical");
+
         Vector2 dir_Move = Vector2.zero;
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (joyaxeX >0 && joyaxeX != 1 || joyaxeX<0 && joyaxeX != -1|| joyaxeY > 0 && joyaxeY != 1 || joyaxeY < 0 && joyaxeY != -1)
         {
-            dir_Move.x = -1f;
+            test = true;
+            dir_Move.x =joyaxeX;
+            dir_Move.y =joyaxeY;
+            player.speedMax = walk;
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else 
         {
-            dir_Move.x = 1f;
+            test = false;
+            dir_Move.x = joyaxeX;
+            dir_Move.y = joyaxeY;
+            player.speedMax = run;
         }
 
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            dir_Move.y = 1f;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            dir_Move.y = -1f;
-        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            entity.Dash();
+            player.Dash();
         }
 
-          entity.Move(dir_Move);
+          player.Move(dir_Move);
     }
 }
