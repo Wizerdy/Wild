0b76be093c2c1db949a333966444db0a1905c917
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-11-04 16:47:48 +0100

    Instinc

---
+++ Wild/Assets/Hugo/Script/SurbrillanceTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurbrillanceTrigger : MonoBehaviour
+{
+    public GameObject triggerzone;
+       
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            trigger();
+        } 
+    }
+
+    public void trigger() 
+    {
+        triggerzone.transform.localScale = new Vector3(3,1,1);
+    }
+}
