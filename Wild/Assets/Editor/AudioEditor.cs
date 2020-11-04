682392477c0281fb3fa1f5c808d574093eeed16a
loickGombauld <wild1907@hotmail.fr>
2020-11-04 16:42:04 +0100

    Soundmanager + ajout du sce,e manager + suppression hdrp

--- Wild/Assets/Editor/AudioEditor.cs
+++ Wild/Assets/Editor/AudioEditor.cs
@@ -10,5 +10,9 @@ public class AudioEditor : Editor
     {
         base.OnInspectorGUI();
         Sound_Manager sm = (Sound_Manager)target;
+        if (GUILayout.Button("Add Sound"))
+        {
+            sm.AddSound();
+        }
     }
 }
--- Wild/Assets/Script_loick/Sound_Manager.cs
+++ Wild/Assets/Script_loick/Sound_Manager.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class Sound_Manager : MonoBehaviour
 {
     public List<SoundExecuter> sound;
-    public AudioClip audioClip;
+    public AudioSource soundtoadd;
     public int index;
+    public soundname soundclass;
     public enum soundname
     {
         SoundEffect,Song
     }
     public void SpawnSE()
     {
-       Instantiate(new GameObject(audioClip.name));
+       Instantiate(new GameObject(sound[index].source.name));
+    }
+    public void AddSound()
+    {
+        int error = 0;
+        for (int i = 0; i < sound.Count; i++)
+        {
+            if (soundtoadd.clip == null || soundtoadd.name == sound[i].source.name)
+            {
+                error++;
+            }
+
+        }
+            if (error>0)
+            {
+                Debug.LogError("Error");
+            }
+        else
+        {
+        SoundExecuter soundadded = new SoundExecuter();
+        soundadded.source = soundtoadd;
+        soundadded.soundtype = soundclass;
+        sound.Add(soundadded);
+        }
     }
 }
 
