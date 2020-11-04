f5dbf21ead3843838dd37d2134d15aab78ddcf18
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-11-04 15:53:13 +0100

    0.1

---
+++ Wild/Assets/Hugo/Script/Surbrillance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surbrillance : MonoBehaviour
+{
+    public Material surbrillance;
+    public Material BaseM;
+    private MeshRenderer Mesh;
+    private bool ShinyOrNot = false;
+    
+    
+    void Start()
+    {
+        Mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    public void Shine() 
+    {
+        ShinyOrNot = !ShinyOrNot;
+
+        if (ShinyOrNot)
+        {
+            Mesh.sharedMaterial = surbrillance;
+        }
+        if (!ShinyOrNot)
+        {
+            Mesh.sharedMaterial = BaseM;
+        }
+    
+    
+    }
+}
