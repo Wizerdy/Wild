894de76471c748d21a5ed294d8e88b2c9bc791ca
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-11-18 11:09:58 +0100

    InstincMultiMaterial

--- Wild/Assets/Hugo/Script/Surbrillance3d.cs
+++ Wild/Assets/Hugo/Script/Surbrillance3d.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class Surbrillance3d : MonoBehaviour
 {
     public Material surbrillance;
+  
     private Material baseM;
     private MeshRenderer Mesh;
-    
+    private List<Material> baseMats = new List<Material>();
+    private List<MeshRenderer> MeshList = new List<MeshRenderer>();
 
 
     void Start()
     {
-        Mesh = GetComponent<MeshRenderer>();
-        baseM = GetComponent<MeshRenderer>().material; 
+        
+        if (transform.childCount == 0)
+        {
+            Mesh = GetComponent<MeshRenderer>();
+            baseM = GetComponent<MeshRenderer>().material;
+
+        }
+        if (transform.childCount !=0)
+        {
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                MeshList.Add(transform.GetChild(i).GetComponent<MeshRenderer>());
+                baseMats.Add(MeshList[i].material);
+            }
+        }
+        
+        
     }
 
     // Update is called once per frame
@@ -26,13 +44,36 @@ public class Surbrillance3d : MonoBehaviour
     public void Shine(bool ShinyOrNot) 
     {
         
-        if (ShinyOrNot)
+        if (transform.childCount == 0)
         {
-            Mesh.sharedMaterial = surbrillance;
+            if (ShinyOrNot)
+            {
+                Mesh.sharedMaterial = surbrillance;
+            }
+            if (!ShinyOrNot)
+            {
+                Mesh.sharedMaterial = baseM;
+            }
         }
-        if (!ShinyOrNot)
+
+        if (transform.childCount != 0)
         {
-            Mesh.sharedMaterial = baseM;
+            
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                
+                if (ShinyOrNot)
+                {
+                    
+                    MeshList[i].material = surbrillance;
+                }
+                if (!ShinyOrNot)
+                {
+                    
+                    MeshList[i].material = baseMats[i];
+                }
+            }
+            
         }
     
     
