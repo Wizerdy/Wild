6dbb00c223db199140f8abfd158ebac779c602f8
loickGombauld <wild1907@hotmail.fr>
2020-11-19 11:45:36 +0100

    Mis en place du soundmanager et du Sound Executer

--- Wild/Assets/Script_loick/SoundExecuter.cs
+++ Wild/Assets/Script_loick/SoundExecuter.cs
@@ -1,27 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class SoundExecuter
+public class SoundExecuter : ScriptableObject
 {
-    public Sound_Manager.soundname soundtype;
+    public Sound_Manager.soundname soundname;
+    public Sound_Manager.soundtype soundtype;
     public AudioClip clip;
-    public void PlaySound()
-    {AudioSource source = new AudioSource();
-        switch (soundtype)
+    public float soundvolume;
+    public AudioSource source;
+    private Sound_Manager SE;
+
+    public SoundExecuter(Sound_Manager sound_Manager)
+    {
+        SE = sound_Manager;
+    }
+
+    public void PlaySound(AudioSource audiosource,SphereCollider sphereCollider,Sound_Manager sound_Manager)
+    {
+        source = audiosource;
+        source.volume = soundvolume;
+        source.clip = clip;
+        switch (soundname)
         {
             case Sound_Manager.soundname.AMB:
+                source.Play();
                 break;
             case Sound_Manager.soundname.RSFX:
+                source.Play();
                 break;
             case Sound_Manager.soundname.MUS:
+                source.loop = true;
+                source.Play();
                 break;
             case Sound_Manager.soundname.NPC:
+                source.Play();
                 break;
             case Sound_Manager.soundname.SFX:
+                source.Play();
                 break;
             case Sound_Manager.soundname.FT:
+                source.Play();
+                sound_Manager.StartCoroutine(GrowSphere(10,sphereCollider));
                 break;
             case Sound_Manager.soundname.LP:
                 source.loop = true;
@@ -29,6 +51,16 @@ public class SoundExecuter
                 break;
             default:
                 break;
+        }
+        
+        
+    }
+    public IEnumerator GrowSphere(int radius,SphereCollider Collision)
+    {
+        for (int i = 0; i < radius; i++)
+        {
+            Collision.radius = i;
+            yield return new WaitForSeconds(0.001f);
         }
     }
 
--- Wild/Assets/Script_loick/Sound_Manager.cs
+++ Wild/Assets/Script_loick/Sound_Manager.cs
@@ -1,27 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Sound_Manager : MonoBehaviour
 {
-    public List<SoundExecuter> sound;
+    public List<SoundExecuter>soundData;
     public AudioClip soundtoadd;
-    public int index;
     public soundname soundclass;
+    public float volume_all;
+    public float volume_sfx;
+    public float volume_mus;
+    //les différents type de son
     public enum soundname
     {
         AMB,RSFX,MUS,NPC,SFX,FT,LP
     }
-    public void SpawnSE()
+    public enum soundtype
     {
-       Instantiate(new GameObject(sound[index].clip.name));
+      ALL,SFX,MUS
     }
+    //fait apparaitre un effet sonore dans la scene
+    public void SpawnSE()
+    { 
+        if (soundclass == soundname.AMB ||soundclass == soundname.RSFX)
+        {
+           StartCoroutine(GenerateSound(soundname.AMB));
+        }
+        else if (soundclass == soundname.FT)
+        {
+            StartCoroutine(GenerateSound(soundclass)); 
+        }
+        else if (soundclass == soundname.LP||soundclass == soundname.MUS)
+        {
+            StartCoroutine(GenerateSound(soundclass)); 
+        }
+        else if (soundclass == soundname.NPC)
+        {
+            StartCoroutine(GenerateSound(soundclass)); 
+        }
+
+    }
+    //ajoute un son à la base de données sonores
     public void AddSound()
     {
         int error = 0;
-        for (int i = 0; i < sound.Count; i++)
+        for (int i = 0; i < soundData.Count; i++)
         {
-            if (soundtoadd.name == null || soundtoadd.name == sound[i].clip.name)
+            if (soundtoadd.name == soundData[i].clip.name)
             {
                 error++;
             }
@@ -33,11 +59,119 @@ public class Sound_Manager : MonoBehaviour
             }
         else
         {
-        SoundExecuter soundadded = new SoundExecuter();
+        SoundExecuter soundadded = new SoundExecuter(this);
         soundadded.clip = soundtoadd;
-        soundadded.soundtype = soundclass;
-        sound.Add(soundadded);
+        soundadded.soundname = soundclass;
+        soundData.Add(soundadded);
+       AssetDatabase.CreateAsset(soundadded,"Assets/Resources/Sound/"+soundadded.clip.name+".asset");
         }
     }
+    //Controle due niveau sonores
+
+
+
+
+
+    public void ManageSound(float sn,soundtype soundtype)
+    {
+        for (int i = 0; i < soundData.Count; i++)
+        {
+            if (soundData[i].soundname == soundclass)
+            {
+                soundData[i].soundvolume = sn;
+            }
+        }
+    }
+
+    //Cooldown avant destruction du son
+
+
+    public IEnumerator GenerateSound(soundname soundtype)
+    {
+        int ran;
+        ran = Random.Range(0, soundData.Count);
+        while (true)
+        {
+            ran = Random.Range(0, soundData.Count);
+            if (soundData[ran].soundname == soundtype)
+            {
+                break;
+            }
+        }
+
+        var obj = new GameObject();
+        obj.AddComponent<Sound>();
+        obj.AddComponent<SphereCollider>();
+        var SC = obj.GetComponent<SphereCollider>();
+
+        if (soundtype != soundname.FT)
+        {
+            DestroyImmediate(SC);
+        }
+
+ 
+   var SO = obj.GetComponent<Sound>();
+       SO.SE = soundData[ran];
+       obj.name = SO.SE.name;
+   var Source = obj.AddComponent<AudioSource>();
+       SO.SE.PlaySound(Source, SC,this);
+       yield return new WaitForSeconds(SO.SE.clip.length);
+        DestroyImmediate(obj);
+        Debug.Log("Done");
+    }
+    public IEnumerator GenerateSound(soundname soundtype, Vector3 vector)
+    {
+        int ran;
+        ran = Random.Range(0, soundData.Count);
+        while (true)
+        {
+            ran = Random.Range(0, soundData.Count);
+            if (soundData[ran].soundname == soundtype)
+            {
+                break;
+            }
+        }
+        var obj = new GameObject();
+        obj.AddComponent<Sound>();
+        obj.AddComponent<SphereCollider>();
+        var SC = obj.GetComponent<SphereCollider>();
+        obj.transform.position = vector;
+        if (soundtype != soundname.FT)
+        {
+            DestroyImmediate(SC);
+        }
+        var SO = obj.GetComponent<Sound>();
+        SO.SE = soundData[ran];
+        obj.name = SO.SE.name;
+        var Source = obj.AddComponent<AudioSource>();
+        SO.SE.PlaySound(Source, SC, this);
+        yield return new WaitForSeconds(SO.SE.clip.length);
+        DestroyImmediate(obj);
+        Debug.Log("Done");
+       
+    }
+
+
+    public void Changevolume_All(float volume) {volume_all = volume;}
+    public void Changevolume_Sfx(float volume) {volume_sfx = volume;}
+    public void Changevolume_Mus(float volume) {volume_mus = volume;}
+
+
+    private void Update()
+    {
+        ManageSound(volume_all, soundtype.ALL); ManageSound(volume_sfx, soundtype.SFX);ManageSound(volume_mus, soundtype.MUS);
+    }
+    private void Start()
+    {
+        SpawnSE();
+    }
+
+
+    public IEnumerator wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("wait sucessfully");
+    }
+
 }
 
