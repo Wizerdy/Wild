f472e62cc56a43e0a904842806e99214864b1fc0
W3R10S <47816763+W3R10S@users.noreply.github.com>
2020-11-28 16:18:56 +0100

    Crocodile WIP

---
+++ Wild/Assets/Scripts/CrocodileEntity.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocodileEntity : AnimalEntity
+{
+    public enum AlertState
+    {
+        NONE, ALERT, HUNT
+    }
+
+    [Header("Field of view")]
+    public int raycastNumber = 2;
+    public float fieldOfView = 90f;
+    public float distanceOfView = 10f;
+
+    [Header("Chase")]
+    public AlertState alertState;
+    public string preyGroupId;
+    //private GameObject prey;
+
+    private void EnterCollision(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            for (int i = 0; i < hitCollider.GetComponent<Entity>().entityGroup.Length; i++)
+            {
+                if (hitCollider.GetComponent<Entity>().entityGroup[i] == preyGroupId)
+                {
+
+                }
+            }
+        }
+    }
+
+    private void ChangeAlert()
+    {
+        if (alertState < AlertState.HUNT)
+        {
+            alertState += 1;
+        } else {
+            alertState = AlertState.NONE;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
