04e252c401d2c86f4c82689b9fb6f34f185de2f1
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-12-04 17:17:41 +0100

    Grass+cloud

---
+++ Wild/Assets/Hugo/Script/GrassAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Lion")
+        {
+            anim.SetBool("Contact", true);
+        }
+        
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Lion")
+        {
+            anim.SetBool("Contact", false);
+        }
+    }
+
+}
