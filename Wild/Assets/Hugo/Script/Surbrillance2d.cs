7deb9bc2af4fd131c2d4c1b6653525ecf27fe330
amir1632 <amir.alotounou@gmail.com>
2020-12-09 12:17:53 +0100

    ajout anim hyene + surbrillance

---
+++ Wild/Assets/Hugo/Script/Surbrillance2d.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surbrillance2d : MonoBehaviour
+{
+    public Material surbrillance;
+    private Material baseM;
+    private SpriteRenderer Sp;
+
+
+
+    void Start()
+    {
+        Sp = GetComponent<SpriteRenderer>();
+        baseM = GetComponent<SpriteRenderer>().material;
+    }
+
+    
+
+    public void Shine(bool ShinyOrNot)
+    {
+
+        if (ShinyOrNot)
+        {
+            Sp.material = surbrillance;
+        }
+        if (!ShinyOrNot)
+        {
+            Sp.material = baseM;
+        }
+
+
+    }
+}
---
+++ Wild/Assets/Hugo/Script/Surbrillance3d.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surbrillance3d : MonoBehaviour
+{
+    public Material surbrillance;
+  
+    private Material baseM;
+    private MeshRenderer Mesh;
+    private List<Material> baseMats = new List<Material>();
+    private List<MeshRenderer> MeshList = new List<MeshRenderer>();
+
+
+    void Start()
+    {
+        
+        if (transform.childCount == 0)
+        {
+            Mesh = GetComponent<MeshRenderer>();
+            baseM = GetComponent<MeshRenderer>().material;
+
+        }
+        if (transform.childCount !=0)
+        {
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                MeshList.Add(transform.GetChild(i).GetComponent<MeshRenderer>());
+                baseMats.Add(MeshList[i].material);
+            }
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    public void Shine(bool ShinyOrNot) 
+    {
+        
+        if (transform.childCount == 0)
+        {
+            if (ShinyOrNot)
+            {
+                Mesh.sharedMaterial = surbrillance;
+            }
+            if (!ShinyOrNot)
+            {
+                Mesh.sharedMaterial = baseM;
+            }
+        }
+
+        if (transform.childCount != 0)
+        {
+            
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                
+                if (ShinyOrNot)
+                {
+                    
+                    MeshList[i].material = surbrillance;
+                }
+                if (!ShinyOrNot)
+                {
+                    
+                    MeshList[i].material = baseMats[i];
+                }
+            }
+            
+        }
+    
+    
+    }
+}
