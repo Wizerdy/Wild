1fc4b0df3a832256aa06ce77c27291889240898d
loickGombauld <wild1907@hotmail.fr>
2020-12-10 21:49:06 +0100

    Mise en place du main Menu

---
+++ Wild/Assets/Script_loick/PreventDeselectionGroup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PreventDeselectionGroup : MonoBehaviour
+{
+   public EventSystem evt;
+
+    private void Start()
+    {
+        evt = EventSystem.current;
+    }
+
+    public GameObject sel;
+
+    private void Update()
+    {
+        if (evt.currentSelectedGameObject != null && evt.currentSelectedGameObject)
+            sel = evt.currentSelectedGameObject;
+        else if (sel != null && evt.currentSelectedGameObject == null)
+            evt.SetSelectedGameObject(sel);
+         if (evt.currentSelectedGameObject.active == false)
+        {
+            evt.SetSelectedGameObject(evt.firstSelectedGameObject);
+        }
+      
+    }
+    public void Select()
+    {
+            sel = evt.firstSelectedGameObject;
+    }
+}
