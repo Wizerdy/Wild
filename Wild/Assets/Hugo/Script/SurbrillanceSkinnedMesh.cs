7454d3b526e58eb2b4b206ac1c6689fcd3fab55a
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-12-11 12:19:51 +0100

    SkinnedMeshInstinct

---
+++ Wild/Assets/Hugo/Script/SurbrillanceSkinnedMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurbrillanceSkinnedMesh : MonoBehaviour
+{
+    public Material surbrillance;
+    private Material baseM;
+    private SkinnedMeshRenderer Sp;
+
+
+
+    void Start()
+    {
+        Sp = GetComponent<SkinnedMeshRenderer>();
+        baseM = GetComponent<SpriteRenderer>().material;
+    }
+
+
+
+    public void Shine(bool ShinyOrNot)
+    {
+
+        if (ShinyOrNot)
+        {
+            Sp.material = surbrillance;
+        }
+        if (!ShinyOrNot)
+        {
+            Sp.material = baseM;
+        }
+
+
+    }
+}
