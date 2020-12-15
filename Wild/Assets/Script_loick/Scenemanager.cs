6bf95832f8065b7476826ab30111f90ea33e4f2a
loickGombauld <wild1907@hotmail.fr>
2020-12-15 20:05:38 +0100

    Optimisation du jeux, ajout de l'occlusion et du temps de chargement

--- Wild/Assets/Script_loick/Scenemanager.cs
+++ Wild/Assets/Script_loick/Scenemanager.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scenemanager : MonoBehaviour
 {
     public string scenename;
+    public Image image;
     public void LoadScene(string scene)
     {
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        Application.backgroundLoadingPriority = ThreadPriority.High;
+        StartCoroutine(LoadGame(scene));
+    }
+
+    IEnumerator LoadGame(string scenename)
+    {
+        AsyncOperation Operation = SceneManager.LoadSceneAsync(scenename);
+        while (!Operation.isDone)
+        {
+            float progress = Mathf.Clamp01(Operation.progress / 0.9f);
+            image.fillAmount = progress;
+            yield return null;
+        }
     }
     public void LoadScene()
     {
